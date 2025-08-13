921808f63657cef1097ca1d458bf20f53aa18acb
Vijay Sharma <sharmavijay@microsoft.com>
2025-08-13 04:25:33 +0000

    Interim change-1

--- show_client/vlan_cli.go
+++ show_client/vlan_cli.go
@@ -9,6 +9,84 @@ const VlanTable= "VLAN"
 const VlanInterfaceTable= "VLAN_INTERFACE"
 const VlanMemberTable= "VLAN_MEMBER"
 
+type VlanBriefColumn struct {
+    Name   string
+    Getter func(cfg VlanConfig, db *DB, vlan string) string
+}
+
+var VlanBriefColumns = []VlanBriefColumn{
+    {"VLAN ID", getVlanId},
+    {"IP Address", getVlanIpAddress},
+    {"Ports", getVlanPorts},
+    {"Port Tagging", getVlanPortsTagging},
+    {"Proxy ARP", getProxyArp},
+}
+
+func getSortedKeys(m map[string]interface{}) []string {
+    keys := make([]string, 0, len(m))
+    for k := range m {
+        keys = append(keys, k)
+    }
+    sort.Strings(keys)
+    return keys
+}
+
+func getVlanId(cfg VlanConfig, db *DB, vlan string) string {
+    return strings.TrimPrefix(vlan, "Vlan")
+}
+
+func getVlanIpAddress(cfg VlanConfig, db *DB, vlan string) string {
+    ipAddress := ""
+    for key, value := range cfg.VlanIpData {
+        ifname, address := parseKey(key)
+        if vlan == ifname {
+            ipAddress += "\n" + address
+        }
+    }
+    return ipAddress
+}
+
+func getVlanPorts(cfg VlanConfig, db *DB, vlan string) string {
+    var vlanPorts []string
+    for key := range cfg.VlanPortsData {
+        portsKey, portsValue := parseKey(key)
+        if vlan != portsKey {
+            continue
+        }
+        vlanPorts = append(vlanPorts, portsValue)
+    }
+    return strings.Join(vlanPorts, "\n")
+}
+
+func getVlanPortsTagging(cfg VlanConfig, db *DB, vlan string) string {
+    var vlanPortsTagging []string
+    for key, value := range cfg.VlanPortsData {
+        portsKey, _ := parseKey(key)
+        if vlan != portsKey {
+            continue
+        }
+        taggingMode := value.(map[string]interface{})["tagging_mode"].(string)
+        vlanPortsTagging = append(vlanPortsTagging, taggingMode)
+    }
+    return strings.Join(vlanPortsTagging, "\n")
+}
+
+func getProxyArp(cfg VlanConfig, db *DB, vlan string) string {
+    proxyArp := "disabled"
+    for key, value := range cfg.VlanIpData {
+        if vlan == key {
+            if v, ok := value.(map[string]interface{})["proxy_arp"]; ok {
+                proxyArp = v.(string)
+            }
+        }
+    }
+    return proxyArp
+}
+
+func parseKey(key interface{}) (string, string) {
+    return "", ""
+}
+
 func getVlanBrief(options sdc.OptionMap) ([]byte, error) {
 	queriesVlan := [][]string{
 		{"CONFIG_DB", VlanTable},
@@ -23,14 +101,42 @@ func getVlanBrief(options sdc.OptionMap) ([]byte, error) {
 	}
 
 	vlanData, derr := GetMapFromQueries(queriesVlan)
+	if derr != nil {
+		log.Errorf("Unable to get data from queries %v, got err: %v", queries, derr)
+		return nil, derr
+	}
+
 	vlanInterfaceData, ierr := GetMapFromQueries(queriesVlanInterface)
+	if ierr != nil {
+		log.Errorf("Unable to get data from queries %v, got err: %v", queries, ierr)
+		return nil, ierr
+	}
+
 	vlanMemberData, merr := GetMapFromQueries(queriesVlanMember)
+	if merr !nil {
+		log.Errorf("Unable to get data from queries %v, got err: %v", queries, merr)
+		return nil, merr
+	}
 
     vlanConfigs := MergeMaps(vlanData, vlanInterfaceData, vlanMemberData)
 
-	if err != nil {
-		log.Errorf("Unable to get data from queries %v, got err: %v", queries, err)
-		return nil, err
-	}
+
+    vlans := getSortedKeys(vlanData)
+    db := nil
+    for _, vlan := range vlans {
+        row := []string{}
+        for _, col := range VlanBriefColumns {
+            row = append(row, col.Getter(vlanConfigs, db, vlan))
+        }
+        body = append(body, row)
+    }
+
+    table := tablewriter.NewWriter(os.Stdout)
+    table.SetHeader(header)
+    for _, v := range body {
+        table.Append(v)
+    }
+    table.Render()
+
 	return data, nil
 }

a94b5dd388ecb1837974cf2ba68d300a4fd088a3
Vijay Sharma <sharmavijay@microsoft.com>
2025-08-14 20:09:41 +0000

    interim change 2

--- show_client/vlan_cli.go
+++ show_client/vlan_cli.go
@@ -5,86 +5,106 @@ import (
 	sdc "github.com/sonic-net/sonic-gnmi/sonic_data_client"
 )
 
-const VlanTable= "VLAN"
-const VlanInterfaceTable= "VLAN_INTERFACE"
-const VlanMemberTable= "VLAN_MEMBER"
+const VlanTable = "VLAN"
+const VlanInterfaceTable = "VLAN_INTERFACE"
+const VlanMemberTable = "VLAN_MEMBER"
+
+type VlanConfig struct {
+	VlanData      map[string]interface{}
+	VlanIpData    map[string]interface{}
+	VlanPortsData map[string]interface{}
+}
 
 type VlanBriefColumn struct {
-    Name   string
-    Getter func(cfg VlanConfig, db *DB, vlan string) string
+	Name   string
+	Getter func(cfg VlanConfig, vlan string) string
 }
 
 var VlanBriefColumns = []VlanBriefColumn{
-    {"VLAN ID", getVlanId},
-    {"IP Address", getVlanIpAddress},
-    {"Ports", getVlanPorts},
-    {"Port Tagging", getVlanPortsTagging},
-    {"Proxy ARP", getProxyArp},
+	{"VLAN ID", getVlanId},
+	{"IP Address", getVlanIpAddress},
+	{"Ports", getVlanPorts},
+	{"Port Tagging", getVlanPortsTagging},
+	{"Proxy ARP", getProxyArp},
 }
 
 func getSortedKeys(m map[string]interface{}) []string {
-    keys := make([]string, 0, len(m))
-    for k := range m {
-        keys = append(keys, k)
-    }
-    sort.Strings(keys)
-    return keys
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
 }
 
-func getVlanId(cfg VlanConfig, db *DB, vlan string) string {
-    return strings.TrimPrefix(vlan, "Vlan")
+// isIPPrefixInKey checks if the key contains an IP address (i.e., is a tuple in Python).
+// In Go, we check if the key is a slice of length >= 2 (since Python tuple is mapped to Go slice).
+func isIPPrefixInKey(key interface{}) bool {
+	switch k := key.(type) {
+	case []interface{}:
+		// Python tuple is often represented as a slice in Go
+		return true
+	default:
+		return false
+	}
 }
 
-func getVlanIpAddress(cfg VlanConfig, db *DB, vlan string) string {
-    ipAddress := ""
-    for key, value := range cfg.VlanIpData {
-        ifname, address := parseKey(key)
-        if vlan == ifname {
-            ipAddress += "\n" + address
-        }
-    }
-    return ipAddress
+func getVlanId(cfg VlanConfig, vlan string) string {
+	return strings.TrimPrefix(vlan, "Vlan")
 }
 
-func getVlanPorts(cfg VlanConfig, db *DB, vlan string) string {
-    var vlanPorts []string
-    for key := range cfg.VlanPortsData {
-        portsKey, portsValue := parseKey(key)
-        if vlan != portsKey {
-            continue
-        }
-        vlanPorts = append(vlanPorts, portsValue)
-    }
-    return strings.Join(vlanPorts, "\n")
+func getVlanIpAddress(cfg VlanConfig, vlan string) string {
+	ipAddress := ""
+	for key, value := range cfg.VlanIpData {
+		if isIPPrefixInKey(key) {
+			ifname, address := parseKey(key)
+			if vlan == ifname {
+				ipAddress += "\n" + address
+			}
+		}
+	}
+	return ipAddress
 }
 
-func getVlanPortsTagging(cfg VlanConfig, db *DB, vlan string) string {
-    var vlanPortsTagging []string
-    for key, value := range cfg.VlanPortsData {
-        portsKey, _ := parseKey(key)
-        if vlan != portsKey {
-            continue
-        }
-        taggingMode := value.(map[string]interface{})["tagging_mode"].(string)
-        vlanPortsTagging = append(vlanPortsTagging, taggingMode)
-    }
-    return strings.Join(vlanPortsTagging, "\n")
+func getVlanPorts(cfg VlanConfig, vlan string) string {
+	var vlanPorts []string
+	for key := range cfg.VlanPortsData {
+		portsKey, portsValue := parseKey(key)
+		if vlan != portsKey {
+			continue
+		}
+		vlanPorts = append(vlanPorts, portsValue)
+	}
+	return strings.Join(vlanPorts, "\n")
 }
 
-func getProxyArp(cfg VlanConfig, db *DB, vlan string) string {
-    proxyArp := "disabled"
-    for key, value := range cfg.VlanIpData {
-        if vlan == key {
-            if v, ok := value.(map[string]interface{})["proxy_arp"]; ok {
-                proxyArp = v.(string)
-            }
-        }
-    }
-    return proxyArp
+func getVlanPortsTagging(cfg VlanConfig, vlan string) string {
+	var vlanPortsTagging []string
+	for key, value := range cfg.VlanPortsData {
+		portsKey, _ := parseKey(key)
+		if vlan != portsKey {
+			continue
+		}
+		taggingMode := value.(map[string]interface{})["tagging_mode"].(string)
+		vlanPortsTagging = append(vlanPortsTagging, taggingMode)
+	}
+	return strings.Join(vlanPortsTagging, "\n")
+}
+
+func getProxyArp(cfg VlanConfig, vlan string) string {
+	proxyArp := "disabled"
+	for key, value := range cfg.VlanIpData {
+		if vlan == key {
+			if v, ok := value.(map[string]interface{})["proxy_arp"]; ok {
+				proxyArp = v.(string)
+			}
+		}
+	}
+	return proxyArp
 }
 
 func parseKey(key interface{}) (string, string) {
-    return "", ""
+	return "", ""
 }
 
 func getVlanBrief(options sdc.OptionMap) ([]byte, error) {
@@ -113,30 +133,29 @@ func getVlanBrief(options sdc.OptionMap) ([]byte, error) {
 	}
 
 	vlanMemberData, merr := GetMapFromQueries(queriesVlanMember)
-	if merr !nil {
+	if merr != nil {
 		log.Errorf("Unable to get data from queries %v, got err: %v", queries, merr)
 		return nil, merr
 	}
 
-    vlanConfigs := MergeMaps(vlanData, vlanInterfaceData, vlanMemberData)
+	vlanCfg := VlanConfig{vlanData, vlanInterfaceData, vlanMemberData}
 
+	vlans := getSortedKeys(vlanData)
+	db := nil
+	for _, vlan := range vlans {
+		row := []string{}
+		for _, col := range VlanBriefColumns {
+			row = append(row, col.Getter(vlanCfg, vlan))
+		}
+		body = append(body, row)
+	}
 
-    vlans := getSortedKeys(vlanData)
-    db := nil
-    for _, vlan := range vlans {
-        row := []string{}
-        for _, col := range VlanBriefColumns {
-            row = append(row, col.Getter(vlanConfigs, db, vlan))
-        }
-        body = append(body, row)
-    }
-
-    table := tablewriter.NewWriter(os.Stdout)
-    table.SetHeader(header)
-    for _, v := range body {
-        table.Append(v)
-    }
-    table.Render()
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader(header)
+	for _, v := range body {
+		table.Append(v)
+	}
+	table.Render()
 
 	return data, nil
 }

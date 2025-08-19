24e33995daa07650851bf9ec97da3bc8b9f4e72b
zitingguo <zitingguo@microsoft.com>
2025-08-19 07:00:14 +0000

    Fix query and construct return format
    Signed-off-by: zitingguo-ms <zitingguo@microsoft.com>

--- show_client/mac_aging_time_cli.go
+++ show_client/mac_aging_time_cli.go
@@ -1,6 +1,9 @@
 package show_client
 
 import (
+	"encoding/json"
+	"fmt"
+
 	log "github.com/golang/glog"
 	sdc "github.com/sonic-net/sonic-gnmi/sonic_data_client"
 )
@@ -14,13 +17,32 @@ admin@sonic:~$ redis-cli -n 0 hget "SWITCH_TABLE:switch" "fdb_aging_time"
 
 func getMacAgingTime(options sdc.OptionMap) ([]byte, error) {
 	queries := [][]string{
-		{"APPL_DB", "SWITCH_TABLE:switch", "fdb_aging_time"},
+		{"APPL_DB", "SWITCH_TABLE", "switch"},
 	}
-	data, err := GetDataFromQueries(queries)
+	data, err := GetMapFromQueries(queries)
 	if err != nil {
 		log.Errorf("Unable to get mac aging time data from queries %v, got err: %v", queries, err)
 		return nil, err
 	}
-	log.Infof("GetDataFromQueries result: %s", string(data))
-	return data, nil
+	log.V(6).Infof("GetMapFromQueries result: %+v", data)
+
+	// Default value if not found
+	agingTime := "N/A"
+
+	if val, ok := data["fdb_aging_time"]; ok && val != nil {
+		strVal := fmt.Sprintf("%v", val)
+		if strVal != "" {
+			agingTime = strVal + "s"
+		} else {
+			log.Warningf("Key 'fdb_aging_time' found but empty in data")
+		}
+	} else {
+		log.Warningf("Key 'fdb_aging_time' not found or empty in data")
+	}
+
+	// Build response, append "s" for seconds
+	result := map[string]string{
+		"fdb_aging_time": agingTime,
+	}
+	return json.Marshal(result)
 }

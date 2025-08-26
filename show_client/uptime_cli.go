9903c17bfa9f110a9a8eafea3d4a18f1a0def447
Vijay Sharma <sharmavijay@microsoft.com>
2025-08-26 21:14:51 +0000

    fix

--- show_client/uptime_cli.go
+++ show_client/uptime_cli.go
@@ -1,16 +1,24 @@
 package show_client
 
 import (
+	"encoding/json"
 	log "github.com/golang/glog"
 	sdc "github.com/sonic-net/sonic-gnmi/sonic_data_client"
 )
 
+type uptimeResponse struct {
+	uptime string `json:"uptime"`
+}
+
 func getUptime(options sdc.OptionMap) ([]byte, error) {
-    uptimeCommandWithParam = "uptime -p"
-	data, err := GetDataFromHostCommand(uptimeCommandWithParam)
+	uptimeCommandWithParam := "uptime -p"
+	uptimeData, err := GetDataFromHostCommand(uptimeCommandWithParam)
 	if err != nil {
 		log.Errorf("Unable to get data uptime, got err: %v", err)
 		return nil, err
 	}
-	return data, nil
+
+	var uptimeResp uptimeResponse
+	uptimeResp.uptime = uptimeData
+	return json.Marshal(uptimeResp)
 }

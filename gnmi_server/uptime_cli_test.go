f50f3f8d68d85750db85fd6d3191a0f83301ee5d
Vijay Sharma <sharmavijay@microsoft.com>
2025-08-26 19:44:35 +0000

    test case

--- gnmi_server/uptime_cli_test.go
+++ gnmi_server/uptime_cli_test.go
@@ -31,7 +31,7 @@ func TestGetUptime(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), QueryTimeout*time.Second)
 	defer cancel()
 
-    expectedUptime := `
+	expectedUptime := `
     {
         "uptime": "up 3 weeks, 4 days, 10 hours, 15 minutes"
     }
@@ -47,28 +47,19 @@ func TestGetUptime(t *testing.T) {
 		wantRespVal    interface{}
 		valTest        bool
 		mockOutputFile map[string]string
-		testTime       time.Time
 		testInit       func()
 	}{
 		{
-			desc:       "query SHOW version without evnironment variable",
+			desc:       "query show uptime with success case",
 			pathTarget: "SHOW",
 			textPbPath: `
 				elem: <name: "version" >
 			`,
 			wantRetCode: codes.OK,
-			wantRespVal: []byte(expectedOutputWithEmptyPlat),
+			wantRespVal: []byte(expectedUptime),
 			valTest:     true,
 			mockOutputFile: map[string]string{
-				"docker": "../testdata/VERSION_DOCKER_IMAGEDATA.txt",
-				"uptime": "../testdata/VERSION_UPTIME.txt",
-			},
-			testTime: time.Date(2025, 7, 18, 18, 0, 0, 0, time.UTC),
-			testInit: func() {
-				MockReadFile(show_client.SonicVersionYamlPath, versionInfo, nil)
-				MockEnvironmentVariable(t, "PLATFORM", "")
-				AddDataSet(t, ConfigDbNum, deviceMetadataFilename)
-				AddDataSet(t, chassisStateDbNum, chassisDataFilename)
+				"uptime": "../testdata/UPTIME.txt",
 			},
 		},
 	}
@@ -82,23 +73,12 @@ func TestGetUptime(t *testing.T) {
 			patches = MockExecCmds(t, test.mockOutputFile)
 		}
 
-		testTime := test.testTime
-		timepatch := gomonkey.ApplyFunc(time.Now, func() time.Time {
-			return testTime
-		})
-
-		show_client.MachineConfPath = "../testdata/VERSION_MACHINE_CONF.conf"
-		show_client.HostDevicePath = "../testdata/"
-
 		t.Run(test.desc, func(t *testing.T) {
 			runTestGet(t, ctx, gClient, test.pathTarget, test.textPbPath, test.wantRetCode, test.wantRespVal, test.valTest)
 		})
 
 		if patches != nil {
 			patches.Reset()
-		}
-		if timepatch != nil {
-			timepatch.Reset()
 		}
 	}
 

d6cccb0f9b4d080460257cfa2ad8f4bbebbc564c
Vijay Sharma <sharmavijay@microsoft.com>
2025-09-04 10:56:33 +0000

    Processes by CPU Implementation

---
+++ show_client/process_cli.py
@@ -0,0 +1,33 @@
+package show_client
+
+import (
+	"encoding/json"
+	"fmt"
+	"sort"
+	"strconv"
+
+	log "github.com/golang/glog"
+	sdc "github.com/sonic-net/sonic-gnmi/sonic_data_client"
+)
+
+const (
+topCommand = "top -bn 1"
+orderByCPU = " -o %CPU"
+)
+
+func loadProcessesDataFromCmdOutput(data string) string {
+    //Store the data in process struct
+}
+
+func getProcesesByCPU(options sdc.OptionMap) ([]byte, error) {
+	cmdForProcessByCPU := topCommand + orderByCPU 
+	
+    processDetails, err := GetDataFromHostCommand(cmdForProcessByCPU)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	processesOrdered := loadProcessesDataFromCmdOutput(cmdForProcessByCPU)
+
+	return json.Marshal(processesOrdered)
+}

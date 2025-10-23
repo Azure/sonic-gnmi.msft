6112b742c2509f1883a33d443fc17081d5194e42
meghna-mishra <meghnamishra@meghnamishra-dev-vm.d4y3nv5wwgfelhhopdxv1tqjld.dx.internal.cloudapp.net>
2025-10-23 08:59:43 +0000

    added state db logic and fixes

--- gnmi_server/feature_status_cli_test.go
+++ gnmi_server/feature_status_cli_test.go
@@ -35,13 +35,12 @@ func TestGetShowFeatureStatus(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), QueryTimeout*time.Second)
 	defer cancel()
 
-	// expected output
-	allFeaturesExpected := `{"features":[{"name":"bgp","data":{"state":"enabled","auto_restart":"enabled","update_time":"2024-10-15 10:30:15","container_id":"bgp_container_123","container_version":"1.2.3","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"database","data":{"state":"enabled","auto_restart":"disabled","update_time":"2024-10-15 10:25:10","container_id":"database_container_456","container_version":"2.1.0","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"lldp","data":{"state":"enabled","auto_restart":"enabled","update_time":"2024-10-15 10:28:45","container_id":"lldp_container_789","container_version":"1.5.2","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"snmp","data":{"state":"enabled","auto_restart":"enabled","update_time":"2024-10-15 10:32:20","container_id":"snmp_container_101","container_version":"3.0.1","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"swss","data":{"state":"enabled","auto_restart":"enabled","update_time":"2024-10-15 10:20:30","container_id":"swss_container_202","container_version":"4.1.5","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"syncd","data":{"state":"enabled","auto_restart":"enabled","update_time":"2024-10-15 10:35:12","container_id":"syncd_container_303","container_version":"2.3.1","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"teamd","data":{"state":"enabled","auto_restart":"enabled","update_time":"2024-10-15 10:27:55","container_id":"teamd_container_404","container_version":"1.8.0","set_owner":"local","current_owner":"local","remote_state":"enabled"}}]}`
+	allFeaturesExpected := `{"features":[{"name":"bgp","data":{"state":"enabled","auto_restart":"enabled","system_state":"Up","update_time":"2024-10-15 10:30:15","container_id":"bgp","container_version":"1.2.3","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"database","data":{"state":"enabled","auto_restart":"disabled","system_state":"Up","update_time":"2024-10-15 10:25:10","container_id":"database","container_version":"2.1.0","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"lldp","data":{"state":"enabled","auto_restart":"enabled","system_state":"Up","update_time":"2024-10-15 10:28:45","container_id":"lldp","container_version":"1.5.2","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"snmp","data":{"state":"enabled","auto_restart":"enabled","system_state":"Up","update_time":"2024-10-15 10:32:20","container_id":"snmp","container_version":"3.0.1","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"swss","data":{"state":"enabled","auto_restart":"enabled","system_state":"Up","update_time":"2024-10-15 10:20:30","container_id":"swss","container_version":"4.1.5","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"syncd","data":{"state":"enabled","auto_restart":"enabled","system_state":"Up","update_time":"2024-10-15 10:35:12","container_id":"syncd","container_version":"2.3.1","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"teamd","data":{"state":"enabled","auto_restart":"enabled","system_state":"Up","update_time":"2024-10-15 10:27:55","container_id":"teamd","container_version":"1.8.0","set_owner":"local","current_owner":"local","remote_state":"enabled"}}]}`
 
-	// expected output for single feature (bgp)
-	bgpFeatureExpected := `{"features":[{"name":"bgp","data":{"state":"enabled","auto_restart":"enabled","update_time":"2024-10-15 10:30:15","container_id":"bgp_container_123","container_version":"1.2.3","set_owner":"local","current_owner":"local","remote_state":"enabled"}}]}`
+	bgpFeatureExpected := `{"features":[{"name":"bgp","data":{"state":"enabled","auto_restart":"enabled","system_state":"Up","update_time":"2024-10-15 10:30:15","container_id":"bgp","container_version":"1.2.3","set_owner":"local","current_owner":"local","remote_state":"enabled"}}]}`
 
 	featureStatusDbDataFilename := "../testdata/FEATURE_DB_DATA.txt"
+	featureStateDbDataFilename := "../testdata/FEATURE_STATE_DB_DATA.txt"
 	featureStatusDbDataEmptyFilename := "../testdata/EMPTY_JSON.txt"
 
 	ResetDataSetsAndMappings(t)
@@ -67,6 +66,7 @@ func TestGetShowFeatureStatus(t *testing.T) {
 			valTest:     false,
 			testInit: func() {
 				AddDataSet(t, ConfigDbNum, featureStatusDbDataEmptyFilename)
+				AddDataSet(t, StateDbNum, featureStatusDbDataEmptyFilename)
 			},
 		},
 		{
@@ -81,7 +81,9 @@ func TestGetShowFeatureStatus(t *testing.T) {
 			valTest:     true,
 			testInit: func() {
 				FlushDataSet(t, ConfigDbNum)
+				FlushDataSet(t, StateDbNum)
 				AddDataSet(t, ConfigDbNum, featureStatusDbDataFilename)
+				AddDataSet(t, StateDbNum, featureStateDbDataFilename)
 			},
 		},
 		{
@@ -143,6 +145,10 @@ func TestGetShowFeatureStatusErrorCases(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), QueryTimeout*time.Second)
 	defer cancel()
 
+	featureStatusDbDataFilename := "../testdata/FEATURE_DB_DATA.txt"
+	featureStateDbDataFilename := "../testdata/FEATURE_STATE_DB_DATA.txt"
+	featureStatusDbDataEmptyFilename := "../testdata/EMPTY_JSON.txt"
+	featureStateDbDataPartialFilename := "../testdata/FEATURE_STATE_DB_DATA_PARTIAL.txt"
 	featureStatusDbDataNoFeatureFilename := "../testdata/EMPTY_JSON.txt"
 
 	ResetDataSetsAndMappings(t)
@@ -168,6 +174,7 @@ func TestGetShowFeatureStatusErrorCases(t *testing.T) {
 			valTest:     false,
 			testInit: func() {
 				AddDataSet(t, ConfigDbNum, featureStatusDbDataNoFeatureFilename)
+				AddDataSet(t, StateDbNum, featureStatusDbDataNoFeatureFilename)
 			},
 		},
 		{
@@ -182,6 +189,79 @@ func TestGetShowFeatureStatusErrorCases(t *testing.T) {
 			valTest:     false,
 			testInit: func() {
 				FlushDataSet(t, ConfigDbNum)
+				FlushDataSet(t, StateDbNum)
+			},
+		},
+		{
+			desc:       "query SHOW feature status with CONFIG_DB only (no STATE_DB)",
+			pathTarget: "SHOW",
+			textPbPath: `
+				elem: <name: "feature" >
+				elem: <name: "status" >
+				elem: <name: "bgp" >
+			`,
+			wantRetCode: codes.OK,
+			wantRespVal: []byte(`{"features":[{"name":"bgp","data":{"state":"enabled","auto_restart":"enabled","system_state":"","update_time":"","container_id":"","container_version":"","set_owner":"local","current_owner":"","remote_state":""}}]}`),
+			valTest:     true,
+			testInit: func() {
+				FlushDataSet(t, ConfigDbNum)
+				FlushDataSet(t, StateDbNum)
+				AddDataSet(t, ConfigDbNum, featureStatusDbDataFilename)
+				AddDataSet(t, StateDbNum, featureStatusDbDataEmptyFilename)
+			},
+		},
+		{
+			desc:       "query SHOW feature status with STATE_DB only (no CONFIG_DB)",
+			pathTarget: "SHOW",
+			textPbPath: `
+				elem: <name: "feature" >
+				elem: <name: "status" >
+			`,
+			wantRetCode: codes.NotFound,
+			wantRespVal: nil,
+			valTest:     false,
+			testInit: func() {
+				FlushDataSet(t, ConfigDbNum)
+				FlushDataSet(t, StateDbNum)
+				AddDataSet(t, ConfigDbNum, featureStatusDbDataEmptyFilename)
+				AddDataSet(t, StateDbNum, featureStateDbDataFilename)
+			},
+		},
+		{
+			desc:       "query SHOW feature status with partial STATE_DB data",
+			pathTarget: "SHOW",
+			textPbPath: `
+				elem: <name: "feature" >
+				elem: <name: "status" >
+				elem: <name: "database" >
+			`,
+			wantRetCode: codes.OK,
+			wantRespVal: []byte(`{"features":[{"name":"database","data":{"state":"enabled","auto_restart":"disabled","system_state":"","update_time":"","container_id":"","container_version":"","set_owner":"local","current_owner":"","remote_state":""}}]}`),
+			valTest:     true,
+			testInit: func() {
+				FlushDataSet(t, ConfigDbNum)
+				FlushDataSet(t, StateDbNum)
+				AddDataSet(t, ConfigDbNum, featureStatusDbDataFilename)
+				// Add STATE_DB with only bgp feature, but query database feature
+				AddDataSet(t, StateDbNum, featureStateDbDataPartialFilename)
+			},
+		},
+		{
+			desc:       "query SHOW feature status all features with mixed STATE_DB data",
+			pathTarget: "SHOW",
+			textPbPath: `
+				elem: <name: "feature" >
+				elem: <name: "status" >
+			`,
+			wantRetCode: codes.OK,
+			wantRespVal: []byte(`{"features":[{"name":"bgp","data":{"state":"enabled","auto_restart":"enabled","system_state":"Up","update_time":"2024-10-15 10:30:15","container_id":"bgp","container_version":"1.2.3","set_owner":"local","current_owner":"local","remote_state":"enabled"}},{"name":"database","data":{"state":"enabled","auto_restart":"disabled","system_state":"","update_time":"","container_id":"","container_version":"","set_owner":"local","current_owner":"","remote_state":""}},{"name":"lldp","data":{"state":"enabled","auto_restart":"enabled","system_state":"","update_time":"","container_id":"","container_version":"","set_owner":"local","current_owner":"","remote_state":""}},{"name":"snmp","data":{"state":"enabled","auto_restart":"enabled","system_state":"","update_time":"","container_id":"","container_version":"","set_owner":"local","current_owner":"","remote_state":""}},{"name":"swss","data":{"state":"enabled","auto_restart":"enabled","system_state":"","update_time":"","container_id":"","container_version":"","set_owner":"local","current_owner":"","remote_state":""}},{"name":"syncd","data":{"state":"enabled","auto_restart":"enabled","system_state":"","update_time":"","container_id":"","container_version":"","set_owner":"local","current_owner":"","remote_state":""}},{"name":"teamd","data":{"state":"enabled","auto_restart":"enabled","system_state":"","update_time":"","container_id":"","container_version":"","set_owner":"local","current_owner":"","remote_state":""}}]}`),
+			valTest:     true,
+			testInit: func() {
+				FlushDataSet(t, ConfigDbNum)
+				FlushDataSet(t, StateDbNum)
+				AddDataSet(t, ConfigDbNum, featureStatusDbDataFilename)
+				// Only BGP has STATE_DB data, others will have empty runtime fields
+				AddDataSet(t, StateDbNum, featureStateDbDataPartialFilename)
 			},
 		},
 	}
--- show_client/feature.go
+++ show_client/feature.go
@@ -38,12 +38,13 @@ func buildFeatureStatusEntry(name string, data map[string]interface{}) map[strin
 		"data": map[string]interface{}{
 			"state":               common.GetValueOrDefault(data, "state", ""),
 			"auto_restart":        common.GetValueOrDefault(data, "auto_restart", ""),
-			"update_time":           common.GetValueOrDefault(data, "update_time", ""),
-			"container_id": common.GetValueOrDefault(data, "container_id", ""),
-			"container_version": common.GetValueOrDefault(data, "container_version", ""),
-			"set_owner":        common.GetValueOrDefault(data, "set_owner", ""),
-			"current_owner": common.GetValueOrDefault(data, "current_owner", ""),
-			"remote_state": common.GetValueOrDefault(data, "remote_state", ""),
+			"system_state":        common.GetValueOrDefault(data, "system_state", ""),
+			"update_time":         common.GetValueOrDefault(data, "update_time", ""),
+			"container_id":        common.GetValueOrDefault(data, "container_id", ""),
+			"container_version":   common.GetValueOrDefault(data, "container_version", ""),
+			"set_owner":           common.GetValueOrDefault(data, "set_owner", ""),
+			"current_owner":       common.GetValueOrDefault(data, "current_owner", ""),
+			"remote_state":        common.GetValueOrDefault(data, "remote_state", ""),
 		},
 	}
 }
@@ -107,11 +108,18 @@ func getFeatureConfig(args sdc.CmdArgs, options sdc.OptionMap) ([]byte, error) {
 
 
 func getFeatureStatus(args sdc.CmdArgs, options sdc.OptionMap) ([]byte, error) {
-	queries := [][]string{{"CONFIG_DB", "FEATURE"}}
+	configQueries := [][]string{{"CONFIG_DB", "FEATURE"}}
+	stateQueries := [][]string{{"STATE_DB", "FEATURE"}}
 
-	rawData, err := common.GetMapFromQueries(queries)
+	configData, err := common.GetMapFromQueries(configQueries)
 	if err != nil {
-		log.Errorf("Unable to get feature status data from queries %v, got err: %v", queries, err)
+		log.Errorf("Unable to get feature config data from queries %v, got err: %v", configQueries, err)
+		return nil, err
+	}
+
+	stateData, err := common.GetMapFromQueries(stateQueries)
+	if err != nil {
+		log.Errorf("Unable to get feature status data from queries %v, got err: %v", stateQueries, err)
 		return nil, err
 	}
 
@@ -121,30 +129,51 @@ func getFeatureStatus(args sdc.CmdArgs, options sdc.OptionMap) ([]byte, error) {
 		featureName = args[0]
 	}
 
-	featureTable := rawData
-
-	if len(featureTable) == 0 {
-		log.Errorf("Failed to fetch data from FEATURE table. Raw data: %+v", rawData)
+	// checking feature table
+	if len(configData) == 0 {
+		log.Errorf("Failed to fetch data from CONFIG_DB FEATURE table. Config data: %+v", configData)
 		return nil, fmt.Errorf("failed to retrieve feature status data")
 	}
 
 	var features []map[string]interface{}
 
 	if featureName != "" {
-		// specific feature
-		if featureData, exists := featureTable[featureName]; exists {
-			if featureDataMap, ok := featureData.(map[string]interface{}); ok {
-				features = append(features, buildFeatureStatusEntry(featureName, featureDataMap))
+		// specific feature 
+		if configFeatureData, exists := configData[featureName]; exists {
+			if configFeatureDataMap, ok := configFeatureData.(map[string]interface{}); ok {
+				mergedData := make(map[string]interface{})
+				if stateFeatureData, stateExists := stateData[featureName]; stateExists {
+					if stateFeatureDataMap, ok := stateFeatureData.(map[string]interface{}); ok {
+						for k, v := range stateFeatureDataMap {
+							mergedData[k] = v
+						}
+					}
+				}
+				for k, v := range configFeatureDataMap {
+					mergedData[k] = v
+				}
+				features = append(features, buildFeatureStatusEntry(featureName, mergedData))
 			}
 		} else {
-			log.Errorf("Feature '%s' not found in FEATURE table. Available features: %v", featureName, common.GetSortedKeys(featureTable))
+			log.Errorf("Feature '%s' not found in CONFIG_DB FEATURE table. Available features: %v", featureName, common.GetSortedKeys(configData))
 			return nil, fmt.Errorf("feature '%s' not found", featureName)
 		}
 	} else {
 		// all features, sorted
-		for _, name := range common.GetSortedKeys(featureTable) {
-			if featureData, ok := featureTable[name].(map[string]interface{}); ok {
-				features = append(features, buildFeatureStatusEntry(name, featureData))
+		for _, name := range common.GetSortedKeys(configData) {
+			if configFeatureData, ok := configData[name].(map[string]interface{}); ok {
+				mergedData := make(map[string]interface{})
+				if stateFeatureData, stateExists := stateData[name]; stateExists {
+					if stateFeatureDataMap, ok := stateFeatureData.(map[string]interface{}); ok {
+						for k, v := range stateFeatureDataMap {
+							mergedData[k] = v
+						}
+					}
+				}
+				for k, v := range configFeatureData {
+					mergedData[k] = v
+				}
+				features = append(features, buildFeatureStatusEntry(name, mergedData))
 			}
 		}
 	}

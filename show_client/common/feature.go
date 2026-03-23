4f250efca02cca8b04a396d5291661cac12533b0
meghna-mishra <meghnamishra@meghnamishra-dev-vm.d4y3nv5wwgfelhhopdxv1tqjld.dx.internal.cloudapp.net>
2026-03-23 08:46:57 +0000

    added code for show asic sdk health event

---
+++ show_client/common/feature.go
@@ -0,0 +1,26 @@
+package common
+
+import (
+	log "github.com/golang/glog"
+)
+
+// CheckFeatureSupported checks if a feature is supported by reading the given
+// field from the specified db, table, and key. Returns true if the field value
+// is "true".
+func CheckFeatureSupported(db, table, key, field string) (bool, error) {
+	queries := [][]string{
+		{db, table, key},
+	}
+	data, err := GetMapFromQueries(queries)
+	if err != nil {
+		log.V(2).Infof("Unable to query %s|%s: %v", table, key, err)
+		return false, nil
+	}
+	if val, ok := data[field]; ok {
+		if strVal, isStr := val.(string); isStr && strVal == "true" {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
